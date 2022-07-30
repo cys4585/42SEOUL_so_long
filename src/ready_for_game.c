5e1601447381984917a4c9c7a7add3b57bf33e77
youngcho <soc4585@naver.com>
2022-07-30 12:04:39 +0900

    Fix: 오탈자

--- src/ready_for_game.c
+++ src/ready_for_game.c
@@ -6,7 +6,7 @@
 /*   By: youngcho <youngcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/29 19:03:33 by youngcho          #+#    #+#             */
-/*   Updated: 2022/07/29 19:54:06 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/07/30 12:03:00 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 void	put_img(t_game_info *game, t_component component, int x, int y)
 {
-	mlx_put_image_to_window(gmae->mlx, game->win, component.img, \
+	mlx_put_image_to_window(game->mlx, game->win, component.img, \
 		x * component.width, y * component.height);
 }
 
 void	put_img_and_cnt_collectible(t_game_info *game, int x, int y)
 {
-	put_img(game->mlx, game->win, game->collectible, x, y);
+	put_img(game, game->collectible, x, y);
 	game->map_info.nbr_collectible++;
 }
 
-void	put_img_set_player_coordinate(t_game_info *game, int x, int y)
+void	put_img_and_set_player_coordinate(t_game_info *game, int x, int y)
 {
-	put_img(game->mlx, game->win, game->player, x, y);
+	put_img(game, game->player, x, y);
 	game->map_info.player_coord.x = x;
 	game->map_info.player_coord.y = y;
 }
@@ -45,13 +45,13 @@ void	set_map_info_and_draw_map(t_game_info *game)
 		while (game->map_info.map_table[y][x])
 		{
 			if (game->map_info.map_table[y][x] == '1')
-				put_img(game->mlx, game->win, game->wall, x, y);
-			else if (map[y][x] == '0')
-				put_img(game->mlx, game->win, game->empty, x, y);
-			else if (map[y][x] == 'C')
+				put_img(game, game->wall, x, y);
+			else if (game->map_info.map_table[y][x] == '0')
+				put_img(game, game->empty, x, y);
+			else if (game->map_info.map_table[y][x] == 'C')
 				put_img_and_cnt_collectible(game, x, y);
 			else if (game->map_info.map_table[y][x] == 'E')
-				put_img(game->mlx, game->win, game->exit, x, y);
+				put_img(game, game->exit, x, y);
 			else if (game->map_info.map_table[y][x] == 'P')
 				put_img_and_set_player_coordinate(game, x, y);
 			x++;
